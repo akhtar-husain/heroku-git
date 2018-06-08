5f035ea363b2207cf9b58b49476f4ad274c8d0dc
Akhtar <akhtar4660@gmail.com>
2018-06-09 02:23:19 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = new express();
+
+app.set('view engine', 'hbs');
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance', {
+        pageTitle: "Maintenance page"
+    });
+});
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        pageTitle: "Homepage",
+        welcomeMessage: "Welcome express..."
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: "About page"
+    });
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
